267f538356a9056054eae3ce61822e9c3511a21b
Erik De Luna <codingerik1.gmail.com>
2020-08-04 11:22:08 -0500

    connecting employee.js

--- controllers/api/employee.js
+++ controllers/api/employee.js
@@ -1,6 +1,8 @@
 // instance of the router 
 const router = require('express').Router();
 
+/// require the db to pull data from models 
+let db = require('../../models')
 
 router.get('/', (req, res) => {
     res.send('hello'); 
