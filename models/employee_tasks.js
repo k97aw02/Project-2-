7e2dc4921a01a3613a3c4f87d2c8a4d44e71f299
Alba Osmanaj <albaos91@gmail.com>
2020-08-03 20:03:38 -0500

    employee tasks many to many relations created

--- models/employee_tasks.js
+++ models/employee_tasks.js
@@ -1,17 +1,28 @@
-
 module.exports = (sequelize, DataTypes) => {
-  const Employee_tasks = sequelize.define("Employee_tasks", {
-    name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        isAlpha: true,
-        notNull: true,
+  const Employee_tasks = sequelize.define(
+    "Employee_tasks",
+    {
+      completed_date: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          isDate: true,
+          notNull: true,
+        },
       },
     },
-  },{
-    freezeTableName: true
-  });
+    {
+      freezeTableName: true,
+    }
+  );
+
+  Employee.associate = function (models) {
+    Employee.belongsToMany(models.Task, { through: models.Employee_tasks });
+  };
+  Task.associate = function (models) {
+    Task.belongsToMany(models.Employee, { through: models.Employee_tasks });
+  };
+
   return Employee_tasks;
 };
 
