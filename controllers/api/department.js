3d421870b36721c51118deeb2e3671138321d213
Erik De Luna <codingerik1.gmail.com>
2020-08-04 11:25:10 -0500

    added the department controller

--- controllers/api/department.js
+++ controllers/api/department.js
@@ -1,13 +1,11 @@
-// instance of the router 
-const router = require('express').Router();
+const router = require('express').Router(); 
 
 /// require the db to pull data from models 
 let db = require('../../models')
 
 router.get('/', (req, res) => {
-    res.send('hello this is the employee route'); 
+    res.send('hello this is the department route'); 
 }); 
-
 
 
 module.exports = router; 
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,10 +2,12 @@
 const router = require('express').Router();
 // all of the routes in this folder 
 const employee = require('./employee');
+const department = require('./department');
 
 
 // api routes
 router.use('/employee', employee);
+router.use('/department', department);
 
 
 module.exports = router;
