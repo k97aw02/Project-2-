a0a52d002a2ceeda59db07a5c4bb44a7ea1b511b
Erik De Luna <codingerik1.gmail.com>
2020-08-05 08:56:06 -0500

    working on employeeTask controller

--- controllers/api/employeeTask.js
+++ controllers/api/employeeTask.js
@@ -4,8 +4,12 @@ const router = require('express').Router();
 /// require the db to pull data from models 
 let db = require('../../models')
 
+// this will get all of the task and people associated
 router.get('/', (req, res) => {
     res.send('hello this is the employeeTask controller'); 
+
+    
+
 }); 
 
 
