1b7e01b338c45a11dafcd1b6288de26f24881a12
Alba Osmanaj <albaos91@gmail.com>
2020-08-05 19:53:17 -0500

    added html route for homepage

--- controllers/htmlRoutes/home.js
+++ controllers/htmlRoutes/home.js
@@ -1,11 +1,19 @@
-const router = require("express").Router(); 
-const path = require('path'); 
+const router = require("express").Router();
+const path = require("path");
 
-// we need to require the models in here 
+router.get("./home", function (req, res) {
+  homepage.all(function (data) {
+    var homeObject = {
+      homepage: data,
+    };
+    console.log(homeObject);
+    res.render("home", homeObject);
+  });
+});
 
-// If no API or html routes are hit send to 
-router.get('/', function (req, res) {
-    res.sendFile(path.join(__dirname, '../../public/home.html'));
+// // If no API or html routes are hit send to
+router.get("/", function (req, res) {
+  res.sendFile(path.join(__dirname, "../../public/home.html"));
 });
 
 // switch this over to handlebars
