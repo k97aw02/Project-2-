2276d0eca392a219de5f10707a2c05456d61a606
Erik De Luna <codingerik1.gmail.com>
2020-08-07 18:16:15 -0500

    added dynamic avatar to the each user

--- public/assets/js/employeePage.js
+++ public/assets/js/employeePage.js
@@ -1,8 +1,21 @@
-$(document).ready(function() {
+$(document).ready(function () {
     // This file just does a GET request to figure out which user is logged in
     // and updates the HTML on the page
-    $.get("/api/login/user_data").then(function(data) {
-      $(".employee-name").text(data.email);
+    $.get("/api/login/user_data").then(function (data) {
+        let user = data.email;
+        $(".employee-name").text(user);
+        // this prefilter was used to correct CORS that prevented the ajax from calling 
+        $.ajaxPrefilter(function (options) {
+            if (options.crossDomain && jQuery.support.cors) {
+                var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+                options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+                // options.url = "http://cors.corsproxy.io/url=" + options.url;
+            }
+        });
+
+        let avatar = 'https://api.adorable.io/avatars/123/';
+        let customeIcon = avatar + user;
+
+        $(".icon").attr('src', customeIcon);
     });
-  });
-  
+});
