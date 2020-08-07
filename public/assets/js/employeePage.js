3437c5e7bd9b74247fd9e8183655c890800a245e
Erik De Luna <codingerik1.gmail.com>
2020-08-07 18:54:25 -0500

    implemented custom output for employee

--- public/assets/js/employeePage.js
+++ public/assets/js/employeePage.js
@@ -4,16 +4,16 @@ $(document).ready(function () {
     $.get("/api/login/user_data").then(function (data) {
         let user = data.email;
         $(".employee-name").text(user);
+
         // this prefilter was used to correct CORS that prevented the ajax from calling 
         $.ajaxPrefilter(function (options) {
             if (options.crossDomain && jQuery.support.cors) {
                 var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
                 options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
-                // options.url = "http://cors.corsproxy.io/url=" + options.url;
             }
         });
 
-        let avatar = 'https://api.adorable.io/avatars/123/';
+        let avatar = 'https://api.adorable.io/avatars/400/';
         let customeIcon = avatar + user;
 
         $(".icon").attr('src', customeIcon);
