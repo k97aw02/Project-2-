f62d92be2a47f07c11e11607454f0a14be4339f9
davenportnw <davenportnw@gmail.com>
2020-08-08 13:50:58 -0500

    Adjust to get working

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,16 +6,10 @@ const department = require('./department');
 const task = require('./task');
 const role = require('./role');
 const employeeTask = require('./employeeTask');
-const register = require('./register');
 
 //! api login 
 const login = require('./login');
 const signup = require('./signup');
-
-//! api login 
-// const login = require('./login');
-// const signup = require('./signup');
-
 
 // api routes
 router.use('/employee', employee);
@@ -23,15 +17,9 @@ router.use('/department', department);
 router.use('/task', task);
 router.use('/role', role);
 router.use('/employeeTask', employeeTask);
-router.use('/register', register);
-
-// ! api login/registration routes
-router.use('/login', login);
 router.use('/signup', signup);
 
-// ! api login/registration routes
-router.use('/login', login);
-router.use('/signup', signup);
+
 
 
 module.exports = router;
