c7dcb8947e82b41c7ed9f8a6682b94b6f74f0cfb
k97aw02 <k97aw02@gmail.com>
2020-10-06 20:31:28 -0500

    edit sequlize

--- config/config.js
+++ config/config.js
@@ -1,35 +1,29 @@
-require('dotenv').config();
-var mysql = require('mysql');
-var connection;
+//require('dotenv').config();
+var mysql = require("mysql");
+
+var connection; 
 
 if (process.env.JAWSDB_URL) {
-     connection = mysql.createConnection(process.env.JAWSDB_URL);
- }
- else {
-module.exports = {
-    development: {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
         username: "root",
-        password: process.env.DB_PASS, /// process.env.password
-        database: "Employee_management",
+        password: "James1189", //process.env.DB_PASS, /// process.env.password
+        database: "employee_management",
         host: "127.0.0.1",
         dialect: "mysql"
-    },
-    test: {
-        username: "root",
-        password: null,
-        database: "database_test",
-        host: "127.0.0.1",
-        dialect: "mysql"
-    },
-    production: {
-        username: "root",
-        password: null,
-        database: "database_production",
-        host: "127.0.0.1",
-        dialect: "mysql"
-    }
-};
- }
-connection.connect();
+
+  })
+}
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+// Export connection for our ORM to use.
 module.exports = connection;
-// sedding
