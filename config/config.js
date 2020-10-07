6e87864c1192129dbedcdad41b92e65e88ca27a2
k97aw02 <k97aw02@gmail.com>
2020-10-07 10:00:03 -0500

    add database name

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports = {
     development: {
       username: "b8zv3pw1y9n7ph1x",
       password: "gqipq3ttsdqfafvn",
-      database: "YOUR JAWS DB DATABASE NAME",
+      database: "kvre1t8innm35w8w",
       host: "durvbryvdw2sjcm5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
       port: 3306,
       dialect: "mysql"
